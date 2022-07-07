8ade48e0074b0f52f46efe177482eee61be5cb71
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-07 23:08:26 +0900

    lib
    // ! lib
    
    // 타입스크립트에게 어떤 API를 사용하고 어떤 환경에서 코드를 실행하는 지를 지정할 수 있습니다.
    // (target 런타임 환경이 무엇인지를 지정합니다.)
    // 프로그램이 브라우저에서 실행되면 lib에 "DOM" 유형 정의를 할 수 있습니다.
    // DOM: window, document 등
    // ex) "lib": ["ES6","DOM"]
    
    // ! 타입 정의(type definition), Declaration Files (정의 파일)
    // 타입 정의는 타입스크립트에게 몇몇 자바스크립트 코드의 타입을 설명해 주기 위해 사용하는 것이다.
    // 타입 정의는 타입스크립트가 몇몇 자바스크립트 코드와 API의 타입을 설명할 수 있도록 해준다.
    // 타입 정의를 사용하는 이유는 타입스크립트를 사용하는 목적과 연결되어 있다.
    // 만약 사용하지 않는다면 자바스크립트로 만들어진 라이브러리를 타입스크립트 프로젝트에 쓰려고 한다면,
    // 타입스크립트는 그것들의 타입에 대해 알 수 없다.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 // const hello = () => "hi";
 
-class Block {
-  constructor(private data: string) {}
-  //   static 메서드를 사용하는 이유는 인스턴스 생성할 필요 없이 클래스의 속성 또는 메서드를 사용하기 위함이다.
-  static hello() {
-    return "hi";
-  }
-}
+// class Block {
+//   constructor(private data: string) {}
+//   //   static 메서드를 사용하는 이유는 인스턴스 생성할 필요 없이 클래스의 속성 또는 메서드를 사용하기 위함이다.
+//   static hello() {
+//     return "hi";
+//   }
+// }
+
+// localStorage.setItem();
+
+import { init, exit } from "myPackage";
+
+init({
+  url: "true",
+});
+
+exit(1);
