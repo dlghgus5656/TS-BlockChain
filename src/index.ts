1874f1e5f6317ed432593c1a727fa08d8e3f20d7
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-08 20:08:13 +0900

    Block
    ts-node, @types/node

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@
 
 // localStorage.setItem();
 
-import { init, exit } from "./myPackage";
+// import { init, exit } from "./myPackage";
 
-init();
+// init();
 
 // init({
 //   url: "true",
@@ -21,3 +21,28 @@ init();
 // exit(1);
 
 // localStorage.clear();
+
+// ! block ------------------------
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  preHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public preHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(preHash, height, data);
+  }
+  static calculateHash(preHash: string, height: number, data: string) {
+    const toHash = `${preHash} ${height} ${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
