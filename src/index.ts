fccde8e036a5e3db3faf228a213abd519694465d
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-10 19:00:10 +0900

    Chain
    Chain

--- src/index.ts
+++ src/index.ts
@@ -33,11 +33,12 @@ interface BlockShape {
 }
 
 class Block implements BlockShape {
-  public hash: string;
+  // 이미 존재하는 블록 업데이트를 readonly를 사용해 막음
+  public readonly hash: string;
   constructor(
-    public preHash: string,
-    public height: number,
-    public data: string
+    public readonly preHash: string,
+    public readonly height: number,
+    public readonly data: string
   ) {
     this.hash = Block.calculateHash(preHash, height, data);
   }
@@ -46,3 +47,33 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPreHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPreHash(), this.blocks.length + 1, data);
+    this.blocks.push(newBlock);
+  }
+  // 이미 존재하는 블록 업데이트를 readonly를 사용해 막음
+  public getBlocks(): readonly Block[] {
+    // ...을 사용해서 배열 안에 데이터를 가진 새로운 배열만 리턴해준다
+    // 악의적으로 추가된 데이터는 추가되지 않음
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
